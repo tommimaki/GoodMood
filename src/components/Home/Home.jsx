abe3adef25e63cbb9887fda7516a331d03279c3b
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-03-14 14:34:48 +0200

    map on homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,7 +27,24 @@ const Home = () => {
         <div className="sliderSection">
           <Slider />
         </div>
-        <ServiceList />
+        <div className="bottomSection">
+          <div className="serviceList">
+            <ServiceList />
+          </div>
+          <div className="map">
+            <h3>location </h3>
+            <iframe
+              title="map"
+              width="100%"
+              height="100%"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62457.30662101689!2d-77.04216320826709!3d-12.149425875047846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7f03d114d83%3A0x1c5f5f740f5e8a53!2sBarranco%2C%20Per%C3%BA!5e0!3m2!1ses-419!2sus!4v1646780892455!5m2!1ses-419!2sus"
+            ></iframe>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
